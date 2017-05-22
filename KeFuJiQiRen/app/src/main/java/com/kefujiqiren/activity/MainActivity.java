fff2c485949112d8118d518a5bed145037df1d97
jswfly <2443243838@qq.com>
2017-05-22 17:44:41 +0800

    表情viewpage添加

--- KeFuJiQiRen/app/src/main/java/com/kefujiqiren/activity/MainActivity.java
+++ KeFuJiQiRen/app/src/main/java/com/kefujiqiren/activity/MainActivity.java
@@ -5,33 +5,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.kefujiqiren.R;
 import com.kefujiqiren.adapter.ChatListViewAdapter;
 import com.kefujiqiren.bean.Msg;
+import com.kefujiqiren.widget.NoScrollViewPager;
+import com.kefujiqiren.widget.StateButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.chatListView)
     ListView chatListView;
-//    @BindView(R.id.editContent)
-//    EditText editContent;
-//    @BindView(R.id.btnSend)
-//    Button btnSend;
+    @BindView(R.id.emotionVoice)
+    ImageButton emotionVoice;
+    @BindView(R.id.editChatText)
+    EditText editChatText;
+    @BindView(R.id.txtVoice)
+    TextView txtVoice;
+    @BindView(R.id.btnEmotion)
+    ImageButton btnEmotion;
+    @BindView(R.id.emotionAdd)
+    ImageView emotionAdd;
+    @BindView(R.id.btnSend)
+    StateButton btnSend;
+    @BindView(R.id.viewpager)
+    NoScrollViewPager viewpager;
+    @BindView(R.id.emotionLayout)
+    RelativeLayout emotionLayout;
     @BindView(R.id.content_main)
     LinearLayout contentMain;
-
     private ChatListViewAdapter adapter;
     private List<Msg> msgList = new ArrayList<>();
 
@@ -42,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        msgList.add(new Msg("今天是个好日子，[微笑][微笑][猪头][猪头]", Msg.TYPE_RECEIVED));
         adapter = new ChatListViewAdapter(this, R.layout.item_custom_service, msgList);
         chatListView.setAdapter(adapter);
 //       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -76,9 +96,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    @OnClick(R.id.btnSend)
-//    public void onViewClicked() {
-//        msgList.add(new Msg(editContent.getText().toString(), Msg.TYPE_SENT));
-//        adapter.notifyDataSetChanged();
-//    }
+    @OnClick({R.id.btnEmotion, R.id.emotionAdd, R.id.btnSend})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnEmotion:
+                break;
+            case R.id.emotionAdd:
+                break;
+            case R.id.btnSend:
+                msgList.add(new Msg(editChatText.getText().toString(), Msg.TYPE_SENT));
+                adapter.notifyDataSetChanged();
+                break;
+        }
+    }
 }
--- KeFuJiQiRen/app/src/main/java/com/kefujiqiren/adapter/ChatListViewAdapter.java
+++ KeFuJiQiRen/app/src/main/java/com/kefujiqiren/adapter/ChatListViewAdapter.java
@@ -2,6 +2,7 @@ package com.kefujiqiren.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,60 +12,83 @@ import android.widget.TextView;
 
 import com.kefujiqiren.R;
 import com.kefujiqiren.bean.Msg;
+import com.kefujiqiren.util.Utils;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by 殇痕 on 2017/5/3.
- *
  */
 
 public class ChatListViewAdapter extends ArrayAdapter<Msg> {
     private int resourceId;
+
     public ChatListViewAdapter(Context context, int resource, List<Msg> objects) {
-        super(context,resource,objects);
+        super(context, resource, objects);
         resourceId = resource;
     }
 
     @NonNull
     @Override
-    public View getView(int position,View convertView,ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         View view;
         ViewHolder holder;
         Msg msg = getItem(position);
-        if(convertView==null){
+        if (convertView == null) {
             view = LayoutInflater.from(getContext()).inflate(resourceId, null, false);
-            holder = new ViewHolder();
-//            holder.headLeft = (RoundedImageView) view.findViewById(R.id.headLeft);
-//            holder.headRight = (RoundedImageView) view.findViewById(R.id.headRight);
-            holder.left = (RelativeLayout) view.findViewById(R.id.left);
-            holder.right = (RelativeLayout) view.findViewById(R.id.right);
-            holder.txtLeft = (TextView) view.findViewById(R.id.txtLeft);
-            holder.txtRight = (TextView) view.findViewById(R.id.txtRight);
+            holder = new ViewHolder(view);
             view.setTag(holder);
-        }else{
+        } else {
             view = convertView;
             holder = (ViewHolder) view.getTag();
         }
 
-        if(msg.getType() == Msg.TYPE_RECEIVED){
+        if (msg.getType() == Msg.TYPE_RECEIVED) {
             holder.left.setVisibility(View.VISIBLE);
             holder.right.setVisibility(View.GONE);
 //            holder.headLeft.setImageResource(msg.get());
-            holder.txtLeft.setText(msg.getContent());
-        }else if(msg.getType() == Msg.TYPE_SENT){
+            SpannableString ss = Utils.getEmotionContent(getContext(), holder.txtRight, msg.getContent());
+            holder.txtLeft.setText(ss);
+        } else if (msg.getType() == Msg.TYPE_SENT) {
             holder.left.setVisibility(View.GONE);
             holder.right.setVisibility(View.VISIBLE);
-            holder.txtRight.setText(msg.getContent());
+            SpannableString ss = Utils.getEmotionContent(getContext(), holder.txtRight, msg.getContent());
+            holder.txtRight.setText(ss);
         }
         return view;
     }
-    class ViewHolder{
+
+    /*static class ViewHolder {
         RelativeLayout left;
         RelativeLayout right;
-//        RoundedImageView headLeft;
+        //        RoundedImageView headLeft;
 //        RoundedImageView headRight;
         TextView txtLeft;
         TextView txtRight;
+    }*/
+
+    class ViewHolder {
+        @BindView(R.id.textTime)
+        TextView textTime;
+        @BindView(R.id.headLeft)
+        RoundedImageView headLeft;
+        @BindView(R.id.txtLeft)
+        TextView txtLeft;
+        @BindView(R.id.left)
+        RelativeLayout left;
+        @BindView(R.id.headRight)
+        RoundedImageView headRight;
+        @BindView(R.id.txtRight)
+        TextView txtRight;
+        @BindView(R.id.right)
+        RelativeLayout right;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
